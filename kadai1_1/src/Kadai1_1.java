c223daccf7d01c6a99c43e69f7ef420d9c87a6ea
tsumugi <tsumugi.1218@gmail.com>
2015-06-02 15:10:02 +0900

    -a

---
+++ kadai1_1/src/Kadai1_1.java
@@ -0,0 +1,123 @@
+import java.io.*;
+
+/*
+ 与えられた16文字からなるべく長い単語を出力する。
+ */
+
+public class Kadai1_1 {
+	
+	public static void main(String[] args) {
+		
+		int size = 235886;
+		
+		String str = "JEANPIErre-xxxxx";
+		
+		String str_small = BigtoSmall(str);   //大文字があったら小文字にする
+		String[] sss = new String[2];
+		String[][] zisyo = new String[2][size];
+		int[] abcde  = new int[27];
+		int[] strabcde = new int[27];
+		int length = str_small.length();
+		int length16 =16;  //与えられた文字列が16と決まっているのでエラー処理するため設定。
+		
+		if(length != length16){
+			System.out.println(length16+"文字入れて下さい！");
+			System.exit(-1);
+		}
+		strabcde = count(str_small); //a~z,z以外の数を数える。
+		int i=0;
+		//System.out.println(length);
+		
+		//ファイル読み込み
+		String FileName = "data/zisyo2.txt";
+		try{
+			BufferedReader br = new BufferedReader(new FileReader(FileName));
+			
+			/* 
+			 1行ずつ読み込み、２次元配列に入れる。
+			  */
+			String line3 = "";
+			while((line3 = br.readLine()) != null){
+				sss = line3.split("	",0);		
+				zisyo[0][i] = sss[0];
+				zisyo[1][i] = sss[1];				
+				i++;				
+			}
+			//ファイル閉じる
+			br.close();
+		}catch(IOException e){
+			System.out.println("Error");
+		}
+		
+		
+		int le = zisyo[0][0].length();
+		int first=0; 
+		while(le != length16){ //文字の長さがlength16のもののうち一番上にあるものの位置を探索。
+			first++;
+			le = zisyo[0][first].length();
+		}
+		//System.out.println(first);
+		
+		int count = first;
+		int ch=0;
+		while(count < size){
+			abcde = count(zisyo[1][count]);
+			/*
+			 入力文字列(strabcde)のa~zの個数を辞書に入ってる文字列(abcde)のa~zの個数と比べて、
+			 abcde内の個数の方が多かったらbreak。	
+			 */		  
+			for(ch=0;ch<27;ch++){
+				if(abcde[ch] > strabcde[ch]){
+					break;
+				}
+			}
+			if(ch ==27){ //a~zの個数が全部、入力文字の方が少なかったら発見！！
+				System.out.println(zisyo[0][count]);
+				break;
+			}
+			
+			count++;
+		}
+
+	}
+		
+
+	
+	/*
+	 文字内のアルファベット数を数える
+	 abc[0]=aの個数、abc[1]=bの個数、abc[2]=bの個数、…　、abc[26]=a~z以外("-"など)の個数
+	 */
+	public static int[] count(String input){ 
+		int[] abc = new int[27];
+		char[] charArray = input.toCharArray();
+		int p;
+		for(int i=0;i<charArray.length;i++){
+			p = charArray[i];
+			if(p>='a' && p <='z'){ //a~z
+				abc[p-'a']++;
+			}
+			else{
+				abc[26]++;  //a~z以外
+			}
+		}
+		return abc;
+	}
+	
+	
+	/*
+	 文字の中に大文字があったら、小文字に
+	 */
+	public static String BigtoSmall(String big){ 
+		char[] small = big.toCharArray();
+		int strlen = big.length();
+		for(int k=0;k<strlen;k++){
+			if(Character.isUpperCase(small[k]) == true){
+				small[k] = Character.toLowerCase(small[k]);
+			}	
+		}
+		return new String(small);
+	}
+	
+	
+
+}
---
+++ kadai1_1/src/MakeZisyo.java
@@ -0,0 +1,142 @@
+import java.io.*;
+import java.util.*;
+
+/*
+ * 
+ zisyo.txtを読み込み、
+ zisyo2.txtは「文字列　ソートした文字列」の形で、ソートした文字列を大きい順に並び替え
+ *
+ */
+
+public class MakeZisyo {
+
+	public static void main(String[] args) {
+		
+		int size = 235886; //辞書の中の文字の数
+		StrString[] array = new StrString[size]; 
+		
+		MakeZisyo outer = new MakeZisyo();
+		int i=0;
+		int length = 0;
+		
+		//ファイルを読み込む
+		String inputFileName = "data/zisyo.txt"; // /usr/share/dict/words
+		String outputFileName2 = "data/zisyo2.txt"; //変更後のファイル(zisyo2.txt)
+		
+		try{			
+			BufferedReader br = new BufferedReader(new FileReader(inputFileName));
+			BufferedWriter bw2 = new BufferedWriter(new FileWriter(outputFileName2));
+			
+			/*「単語　ソートしたもの」を１行として書き込む*/
+			String line = "";
+			i=0;
+		    length = 0;
+			while((line = br.readLine()) != null){
+				length = line.length();
+
+				array[i] = outer.new StrString(length,line,sort(BigtoSmall(line)));
+				
+				i++;
+			}
+			
+			
+			StrStringComparator comparator = outer.new StrStringComparator(StrStringComparator.LEN);
+			
+			/*並び替え*/
+			Arrays.sort(array, comparator);
+								
+			//ファイル書き込み
+			for(int k=0;k<size;k++){
+				bw2.write(array[k].s1);
+				bw2.write("	");
+			    bw2.write(array[k].s2);
+				bw2.newLine();
+			}
+			
+			//ファイルを閉じる
+			br.close(); 
+			bw2.close();
+
+		}catch(IOException e){
+			System.out.println("Error");
+		}
+		
+	}
+	
+	public static String BigtoSmall(String big){ //大文字があったら小文字にする
+		char[] small = big.toCharArray();
+		int strlen = big.length();
+		for(int k=0;k<strlen;k++){
+			if(Character.isUpperCase(small[k]) == true){
+				small[k] = Character.toLowerCase(small[k]);
+			}	
+		}
+		return new String(small);
+	}
+	
+	public static String sort(String before){ //ソートする
+		char[] after = before.toCharArray();
+		java.util.Arrays.sort(after);
+		return new String(after);		
+	}
+	
+	public static int[] count(String input){ //文字内のアルファベット数を数える
+		int[] abc = new int[27];
+		char[] charArray = input.toCharArray();
+		int p;
+		for(int i=0;i<charArray.length;i++){
+			p = charArray[i];
+			if(p>='a' && p <='z'){
+				abc[p-'a']++;
+			}
+			else{
+				abc[26]++;
+			}
+		}
+		return abc;
+	}
+
+
+	
+	class StrStringComparator implements Comparator<StrString>{
+		private final int key;
+		public final static int LEN = 0;
+		public final static int STR1 = 1;
+		public final static int STR2 = 2;
+		
+		public StrStringComparator(int key){
+			super();
+			this.key =key;
+		}
+		
+		public int compare(StrString o1,StrString o2){
+			switch(key){
+				default:
+				case LEN:
+					return o2.le.compareTo(o1.le); //大きい順
+				case STR1:
+					return o1.s1.compareTo(o2.s1);
+				case STR2:
+					return o1.s2.compareTo(o2.s2);
+			}
+		}
+
+	}
+
+	
+	class StrString{
+		Integer le;
+		String s1;
+		String s2;
+		
+		public StrString(Integer le,String s1,String s2){
+			super();
+			this.le =le;
+			this.s1 =s1;
+			this.s2 =s2;
+		}
+	}
+	
+	
+
+}
